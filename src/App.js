5b753a43a1775fdfc5f68f942a8c0710ecfd111c
ZhivkoZhelyazkov <zkzhelyazkov@gmail.com>
2021-11-23 10:46:48 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import * as authService from './services/authService';
+
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import MyPets from './components/MyPets/MyPets';
+import Create from './components/Create/Create';
+import Details from './components/Details/Details';
+import Edit from './components/Edit/Edit';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userInfo, setUserInfo] = useState({ isAuthenticated: false, username: '' });
+
+    useEffect(() => {
+        let user = authService.getUser();
+
+        setUserInfo({
+            isAuthenticated: Boolean(user),
+            user
+        });
+    }, []);
+
+    const onLogin = (username) => {
+        setUserInfo({
+            isAuthenticated: true,
+            user: username
+        });
+    };
+
+    return (
+        <div id="container">
+
+            <Header {...userInfo} />
+
+            <main id="site-content">
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/login" element={<Login onLogin={onLogin} />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/my-pets" element={<MyPets />} />
+                    <Route path="/create" element={<Create />} />
+
+                    {/* <Details />
+                    <Edit /> */}
+                </Routes>
+            </main>
+
+            <footer id="site-footer">
+                <p>@PetMyPet</p>
+            </footer>
+
+        </div>
+    );
 }
 
 export default App;
