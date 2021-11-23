df79bea0e1e06ab01101bc9025e7275baaaa2d42
ZhivkoZhelyazkov <zkzhelyazkov@gmail.com>
2021-11-23 15:45:03 +0200

    Add pet

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import PetList from '../PetList';
+
+const Dashboard = () => {
+    return (
+        <section id="dashboard-page" className="dashboard">
+            <h1>Dashboard</h1>
+            <nav>
+                <Link to="types">Types</Link>
+            </nav>
+            <section>
+                <Routes>
+                    <Route path="/" element={<PetList />} />
+                    <Route path="/types" element={<p>Types ...</p>} />
+                </Routes>
+            </section>
+        </section>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+const Header = ({
+    isAuthenticated,
+    user
+}) => {
+    let guestNavigation = (
+        <div id="guest">
+            <Link className="button" to="/login">Login</Link>
+            <Link className="button" to="/register">Register</Link>
+        </div>
+    );
+
+    let userNavigation = (
+        <div id="user">
+            <span>Welcome, {user}</span>
+            <Link className="button" to="/my-pets">My Pets</Link>
+            <Link className="button" to="/create">Add Pet</Link>
+            <Link className="button" to="#">Logout</Link>
+        </div>
+    );
+
+    return (
+        <header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <Link to="/dashboard">Dashboard</Link>
+                    {isAuthenticated ? userNavigation : guestNavigation}
+                </section>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
